11eb33fd231d1c741e01e6fff9650d84454b6200
NotAShelf <itsashelf@gmail.com>
2023-04-26 00:15:52 +0300

    separate stable and unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,15 +3,18 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # based unstable packages
+    nixpkgs-master.url = "github:nixos/nixpkgs/master"; # cringe stable packages
 
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # neovim-flake
+    neovim-flake = {
+      url = "github:notashelf/neovim-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
