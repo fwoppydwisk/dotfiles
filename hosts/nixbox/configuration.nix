5df643d333561e67c8700b258d9a4e66e120eaec
Frankie B <git@diskfloppy.me>
2023-08-21 22:41:26 +0100

    what the fuck

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -75,9 +75,7 @@
   # Enable experimental features
   nix.settings.extra-experimental-features = ["flakes" "nix-command"];
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  # I never want to touch this again
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -85,10 +83,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
--- hosts/nixpad/configuration.nix
+++ hosts/nixpad/configuration.nix
@@ -68,9 +68,7 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  # I never want to touch this again
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -78,10 +76,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   services.xserver.videoDrivers = ["intel"];
