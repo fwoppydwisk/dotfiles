a944f4ab5238a8985bd800bc98a015ec864a7dc4
Frankie B <git@diskfloppy.me>
2023-09-02 19:46:44 +0100

    Add hyprland home manager module to nixpad

--- flake.nix
+++ flake.nix
@@ -92,6 +92,8 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
+          hyprland.homeManagerModules.default
+          {wayland.windowManager.hyprland.enable = true;}
           # > Our main home-manager configuration file <
           ./homes/floppydisk
         ];
