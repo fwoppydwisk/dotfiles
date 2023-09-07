47d826abe179cada0319b90cf229bf8dac2b926d
Frankie B <git@diskfloppy.me>
2023-09-07 14:29:40 +0100

    Format hyprland.nix

--- homes/floppydisk/hyprland.nix
+++ homes/floppydisk/hyprland.nix
@@ -35,10 +35,10 @@
 
   decoration = {
     blur = {
-  enabled = true;
-  size = 3;
-  passes = 1;
-  new_optimizations = 1;
+      enabled = true;
+      size = 3;
+      passes = 1;
+      new_optimizations = 1;
     };
     drop_shadow = "yes";
     shadow_range = 20;
@@ -49,12 +49,12 @@
   animations = {
     enabled = true;
     animation = [
-  "windows, 1, 7, default"
-  "windowsOut, 1, 7, default, popin 80%"
-  "border, 1, 10, default"
-  "borderangle, 1, 8, default"
-  "fade, 1, 7, default"
-  "workspaces, 1, 6, default"
+      "windows, 1, 7, default"
+      "windowsOut, 1, 7, default, popin 80%"
+      "border, 1, 10, default"
+      "borderangle, 1, 8, default"
+      "fade, 1, 7, default"
+      "workspaces, 1, 6, default"
     ];
   };
 
