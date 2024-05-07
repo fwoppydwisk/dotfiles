980b77144636f15fa995592dc5db25625c004acc
Frankie B. <git@diskfloppy.me>
2024-05-07 19:46:27 +0100

    Fix fprintd

--- hosts/watermelon/configuration.nix
+++ hosts/watermelon/configuration.nix
@@ -72,7 +72,26 @@
     pulse.enable = true;
   };
 
+  security.polkit.enable = true;
+
+  systemd = {
+    user.services.polkit-lxqt = {
+      description = "polkit-lxqt";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
+    lxqt.lxqt-policykit
     gcc
     git
     grim
