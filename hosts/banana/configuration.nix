43a11e67dbccc082d062bcb15396abb17577be68
rubiigen <alyxwren@proton.me>
2024-07-11 15:14:25 +0100

    forgot the change to hardware.graphics from hardware.opengl in banana's configuration.nix file, added.

--- hosts/banana/configuration.nix
+++ hosts/banana/configuration.nix
@@ -22,10 +22,9 @@
 
   services.tailscale.enable = true;
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   nixpkgs = {
